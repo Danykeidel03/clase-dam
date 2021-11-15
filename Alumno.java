f8c393a12ad45af270504854501f3ee9ee3b8748
Daniel <supertuxcar@gmail.com>
2021-11-15 14:02:18 +0100

    mayorYMenor

--- Alumno.java
+++ Alumno.java
@@ -1,23 +1,25 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
     private String numeroMatricula;
     // la edad del alumno
     private int edad;
- 
 
     /**
      * Crea un alumno nuevo
      */
     public Alumno(String nombreCompleto, String numeroMatriculaAlumno, int edadAlumno) {
-       
-       nombre = nombreCompleto;
+        nombre = nombreCompleto;
         numeroMatricula = numeroMatriculaAlumno;
         edad = edadAlumno;
-        if (nombre.length() < 3 ) {System.out.println("Se produjo un error"); }
-        if (numeroMatricula.length() < 4 ) {System.out.println("Se produjo un error"); }
+        if (nombre.length() < 3 ) {
+            System.out.println("Se produjo un error"); 
+        }
+        if (numeroMatricula.length() < 4 ) {
+            System.out.println("Se produjo un error"); 
+        }
     }
 
     /**
@@ -26,29 +28,41 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
-        System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " aÃ±os");
+        System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
-        int length; 
-        return nombre.substring(0, 4) + numeroMatricula.substring(0, 5);
+        String nombreADevolver = "";
+        if (nombre.length() < 3 ) {
+            nombreADevolver = nombre;
+        } 
+        else {
+            nombreADevolver = nombre.substring(0, 3);
+        }
+        String numeroADevolver = "";
+        if (numeroMatricula.length() < 4 ) {
+            numeroADevolver = numeroMatricula;
+        } 
+        else {
+            numeroADevolver = numeroMatricula.substring(0, 4);
+        }
+        return nombreADevolver + numeroADevolver;
     }
-    
 
 }
